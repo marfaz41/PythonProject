5a4ca215b1440263a131071862c05b44e8dfff39
Marfaz41 <marzooqabdurazaq41@gmail.com>
2026-07-28 22:27:40 +0100

    lesson 43 - Mad Libs Game

--- main.py
+++ main.py
@@ -1,54 +1,16 @@
-# This is my first python programme
-print("i like rice")
-print("it`s really good!")
-print("I love fauziyah")
-# strings
-first_name = "Marzooq"
-food = "rice"
-email = "marzooqabdurazaq41@gmail.com"
-hobby = "coding"
-spouse = "fauziyah"
-school = "GTC"
-#integers
-age = 18
-quantity = 3
-num_of_sudents = 45
+# Madlibs game
+# Word game where you acreate a story
+# by filling in blanks with random words
 
-print("you are {18} years old")
-print("you are buying {3} items")
-print(f" your class has {num_of_sudents} students")
-# floats
-price = 4500
-gpa = 3.2
-distance = 44
-lenght = 50
-print(f"The price is N{price}")
-print(f" your gpa is {gpa}")
-print(f" you sprint {distance}km")
-print(F" the lenght {lenght}m")
-# boolean
+adjective1 = input ("Enter an adjective (describtion): ")
+noun1 = input ("Enter a noun (person, place thing): ")
+adjective2 = input ("Enter an adjective (describtion): ")
+verb1 = input("Enter a verb ending with 'ing'")
+adjective3 = input ("Enter an adjective (describtion): ")
 
-is_student = True
-for_sale = False
-is_online = False
-he_is_an_admin = True
-if he_is_an_admin:
 
-    print("he is an admin")
-else:print("he is not an admin")
-if is_online:
-    print("You are online")
-else:
-        print("You are NOT online")
+print(f" Today i went to a {adjective1} zoo.")
+print(f"In exhibit, i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f" i was {adjective3}!")
 
-if is_student:
-    print("You are a student")
-else:
-    print("You are not a student")
-    #
-    name = "Abdulrazaq marizooq"
-    age = 18
-    gpa = 4.9
-    is_student = True
-
-    print(type(name))
