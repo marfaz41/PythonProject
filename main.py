e8700f8b16a4c762e1723280220619d6fab83cec
Marfaz41 <marzooqabdurazaq41@gmail.com>
2026-07-30 22:06:59 +0100

    lesson 44 - Math Logrithms

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# Madlibs game
-# Word game where you acreate a story
-# by filling in blanks with random words
+import math
 
-adjective1 = input ("Enter an adjective (describtion): ")
-noun1 = input ("Enter a noun (person, place thing): ")
-adjective2 = input ("Enter an adjective (describtion): ")
-verb1 = input("Enter a verb ending with 'ing'")
-adjective3 = input ("Enter an adjective (describtion): ")
+a = float(input("Enter side A: "))
+b = float(input("Enter side b: "))
 
+c = math.sqrt(pow(a, 2) + pow(b, 2))
 
-print(f" Today i went to a {adjective1} zoo.")
-print(f"In exhibit, i saw a {noun1}")
-print(f"{noun1} was {adjective2} and {verb1}")
-print(f" i was {adjective3}!")
-
+print (f"Side C = {c}")
