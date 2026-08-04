679b4d21427aac88de496f1cdda065759b236276
Marfaz41 <marzooqabdurazaq41@gmail.com>
2026-08-04 20:16:51 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-# python weight converter
 
-weight = float(input("Enter your weight"))
-unit = input("Kilograms or pounds? (K or L): ")
+unit = input("Is this temprature in celsius or fahremheight (C/F): ")
+temp = float(input("Enter the temprature: "))
 
-if unit =="K":
-     weight = weight * 2.205
-     unit == "lbs."
-     print(F"Your weight is: {round(weight, 1)} {unit}")
-elif unit == "L":
-     weight = weight / 2.205
-     unit == "kgs."
-     print(F"Your weight is: {round(weight, 1)} {unit}")
-
+if unit == "C":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"The temprature in fahremheight is: {temp}"f" ")
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9)
+    print(f"The temprature in celcius is: {temp}C")
 else:
-     print(F"{unit} was not valid")
-
+     print(f"{unit} is an invalid unit of measurement")
