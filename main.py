919f4a2361a6d11b71604f54af637873253c2dff
Marfaz41 <marzooqabdurazaq41@gmail.com>
2026-08-04 12:05:21 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,54 +1,24 @@
-# This is my first python programme
-print("i like rice")
-print("it`s really good!")
-print("I love fauziyah")
-# strings
-first_name = "Marzooq"
-food = "rice"
-email = "marzooqabdurazaq41@gmail.com"
-hobby = "coding"
-spouse = "fauziyah"
-school = "GTC"
-#integers
-age = 18
-quantity = 3
-num_of_sudents = 45
+ # python calculator
 
-print("you are {18} years old")
-print("you are buying {3} items")
-print(f" your class has {num_of_sudents} students")
-# floats
-price = 4500
-gpa = 3.2
-distance = 44
-lenght = 50
-print(f"The price is N{price}")
-print(f" your gpa is {gpa}")
-print(f" you sprint {distance}km")
-print(F" the lenght {lenght}m")
-# boolean
+operator = input("Enter an operator(= - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
 
-is_student = True
-for_sale = False
-is_online = False
-he_is_an_admin = True
-if he_is_an_admin:
+if operator == "+":
+     result = num1 + num2
+     print(result)
+elif operator == "-":
+     result = num1 - num2
+     print(result)
+elif operator == "*":
+     result = num1 - num2
+     print(result)
+elif operator == "/":
+     result = num1 - num2
+     print(result)
 
-    print("he is an admin")
-else:print("he is not an admin")
-if is_online:
-    print("You are online")
-else:
-        print("You are NOT online")
 
-if is_student:
-    print("You are a student")
-else:
-    print("You are not a student")
-    #
-    name = "Abdulrazaq marizooq"
-    age = 18
-    gpa = 4.9
-    is_student = True
 
-    print(type(name))
+
+
+
